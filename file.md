e6df3fb9e494f95399f93c56aa56561c04138551
lens <okorelens27@gmail.com>
2023-06-09 04:59:56 +0300

    Initialised our project

---
+++ file.md
@@ -0,0 +1,11 @@
+## web 1
+
+Display Data to user
+
+## Web 2
+
+User Interact with data ---
+forms
+
+        DOM manipulation
+            JS
---
+++ playground/app.js
@@ -0,0 +1,26 @@
+// declaring variables
+// const ,let ,var
+
+// const list = document.getElementById("list");
+// const smth = document.getElementsByClassName("list");
+
+// const querySelector = document.querySelector("ul");
+// const querySelectorAll = document.querySelectorAll("li");
+
+// console.log(querySelectorAll.length);
+
+// let names = "lens";
+
+// names = "otiato";
+
+// const button = document.getElementById("btn");
+
+// const faith = document.getElementById("eugene");
+
+// function changeName() {
+// alert(faith.innerText);
+// faith.innerText = "Intrudor";
+// }
+
+// we should point to the function
+// button.addEventListener("click", changeName);
